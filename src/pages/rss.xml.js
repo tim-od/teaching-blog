55368a955ff0a79285a321a3db867384c8a88a58
tim-od <132780893+tim-od@users.noreply.github.com>
2026-08-01 15:09:55 +0100

    Add RSS feed

---
+++ src/pages/rss.xml.js
@@ -0,0 +1,18 @@
+import rss from '@astrojs/rss';
+import { getCollection } from 'astro:content';
+
+export async function GET(context) {
+  const posts = await getCollection('posts');
+  return rss({
+    title: 'Second Chalk',
+    description: 'A career-change blog for UK professionals considering a move into teaching.',
+    site: context.site,
+    items: posts.map((post) => ({
+      title: post.data.title,
+      description: post.data.description,
+      pubDate: post.data.pubDate,
+      link: `/posts/${post.id}/`,
+    })),
+    customData: `<language>en-gb</language>`,
+  });
+}
