c1051aa184f1ab43c8b42c46e9da2809abbc1877
tim-od <132780893+tim-od@users.noreply.github.com>
2026-08-01 15:02:42 +0100

    Add sitemap integration and site URL

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,5 +1,10 @@
 // @ts-check
 import { defineConfig } from 'astro/config';
 
+import sitemap from '@astrojs/sitemap';
+
 // https://astro.build/config
-export default defineConfig({});
+export default defineConfig({
+  site: 'https://teaching-blog.tim-e74.workers.dev',
+  integrations: [sitemap()]
+});
