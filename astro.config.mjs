ebc654f23f1cf757957f7feaa8d2af16689deb4b
tim-od <132780893+tim-od@users.noreply.github.com>
2026-08-02 18:35:10 +0100

    Update site URL to secondbell.co.uk

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,10 +1,9 @@
-// @ts-check
 import { defineConfig } from 'astro/config';
 
 import sitemap from '@astrojs/sitemap';
 
 // https://astro.build/config
 export default defineConfig({
-  site: 'https://teaching-blog.tim-e74.workers.dev',
+  site: 'https://secondbell.co.uk',
   integrations: [sitemap()]
 });
